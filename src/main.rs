c5cdd8f105d93d6bf2d4a70e6841856de8e4f36e
Travis Dockter <travisdock@gmail.com>
2021-05-13 16:01:06 -0600

    Sends encrypted

--- src/main.rs
+++ src/main.rs
@@ -19,27 +19,27 @@ fn main() {
     let mut client = TcpStream::connect(LOCAL).expect("Stream failed to connect");
     client.set_nonblocking(true).expect("failed to initiate non-blocking");
 
-    let (tx, rx) = mpsc::channel::<String>();
+    let (tx, rx) = mpsc::channel::<Vec<u8>>();
 
     thread::spawn(move || loop {
         let mut buff = vec![0; MSG_SIZE];
         match client.read_exact(&mut buff) {
             Ok(_) => {
                 let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-                let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+                // let msg = String::from_utf8(msg).expect("Invalid utf8 message");
                 stdout().execute(MoveDown(1)).expect("failed move cursor");
-                println!("{}", msg);
+                println!("{:?}", msg);
             },
             Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
-            Err(_) => {
-                println!("connection with server was severed");
+            Err(e) => {
+                println!("ERROR: {}", e);
                 break;
             }
         }
 
         match rx.try_recv() {
             Ok(msg) => {
-                let mut buff = msg.clone().into_bytes();
+                let mut buff = msg.clone();
                 buff.resize(MSG_SIZE, 0);
                 client.write_all(&buff).expect("writing to socket failed");
                 // println!("message sent {:?}", msg);
@@ -56,27 +56,24 @@ fn main() {
         let mut buff = String::new();
         io::stdin().read_line(&mut buff).expect("reading from stdin failed");
         stdout().execute(MoveUp(2)).expect("failed move cursor");
-        let msg = format!("{}: {}", USERNAME, buff.trim()).to_string();
-        // encrypt_message(&msg);
-        if msg == ":quit" || tx.send(msg).is_err() {break}
+        // let msg = format!("{}: {}", USERNAME, buff.trim()).to_string();
+        let msg = encrypt_message(&buff);
+        tx.send(msg).expect("You fucked up");
     }
     println!("bye bye!");
 
 }
 
 
-// fn encrypt_message(msg: &str) -> Message {
-//     let mut file = File::open("test_public.pem").unwrap();
-//     let mut contents = String::new();
-//     file.read_to_string(&mut contents).unwrap();
-//     let rsa = Rsa::public_key_from_pem(contents.as_bytes()).unwrap();
-//     let mut buf: Vec<u8> = vec![0; rsa.size() as usize];
-//     rsa.public_encrypt(msg.as_bytes(), &mut buf, Padding::PKCS1).unwrap();
-//     let to = String::from("test");
-//     let from = String::from("test");
-
-//     Message::new(to, from, buf)
-// }
+fn encrypt_message(msg: &str) -> Vec<u8> {
+    let mut file = File::open("test_public.pem").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let rsa = Rsa::public_key_from_pem(contents.as_bytes()).unwrap();
+    let mut buf: Vec<u8> = vec![0; rsa.size() as usize];
+    rsa.public_encrypt(msg.as_bytes(), &mut buf, Padding::PKCS1).unwrap();
+    buf
+}
 
 // fn decrypt_message(msg: Message, passphrase: &str) -> String {
 //     let mut file = File::open("test_private.pem").unwrap();
